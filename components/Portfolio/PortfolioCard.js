bae7f23afe8dc8fe9b91178325f43fca4761b154
arharuty <13937@coderacademy.edu.au>
2023-12-19 22:48:19 +1100

    Revert "update dummy text in portfolio"
    This reverts commit 9954b93966e0493a019b40753b9c543029928869.
    
    revert

--- components/Portfolio/PortfolioCard.js
+++ components/Portfolio/PortfolioCard.js
@@ -7,25 +7,25 @@ const PortfolioCard = () => {
     {
       name: "OHA Aikman Club",
       link: "https://www.aikman.club/",
-      desc: "The Aikman Club was founded in 2015 as part of the Old Haileyburian Association (OHA) who continue to support the Club. Anyone interested in participating in rowing is welcome to join at the National Water Sports Centre.",
+      desc: "Lorem Ipsum is simply dummy text, Lorem Ipsum is simply a placeholder for Lorem Ipsum. Lorem Ipsum may be used as a placeholder for any number of other characters",
       logo: "/images/client-logos/AC.jpg",
     },
     {
       name: "Melbourne Social Tango",
       link: "https://melbournesocialtango.com/",
-      desc: "Melbourne Social Tango is a community of tango dancers who practice, share ideas and skills, and dance Argentine social tango in welcome and inclusive settings.",
+      desc: "Lorem Ipsum is simply dummy text, Lorem Ipsum is simply a placeholder for Lorem Ipsum. Lorem Ipsum may be used as a placeholder for any number of other characters",
       logo: "/images/client-logos/MST.png",
     },
     {
       name: "Lifestyle Investment Planning",
       link: "https://www.lifestyleip.com.au/",
-      desc: "Lifestyle was created out of a desire to improve the financial wellbeing of people. They provide financial advice in areas such as superannuation, retirement planning, estate planning, savings goals, and much more.",
+      desc: "Lorem Ipsum is simply dummy text, Lorem Ipsum is simply a placeholder for Lorem Ipsum. Lorem Ipsum may be used as a placeholder for any number of other characters",
       logo: "/images/client-logos/LifestyleIP.png",
     },
     {
       name: "Dominium",
       link: "https://www.dominium.com.au/",
-      desc: "Dominium provides buyers advocacy services with experience in assessing and purchasing residential, commercial, industrial and rural property",
+      desc: "Lorem Ipsum is simply dummy text, Lorem Ipsum is simply a placeholder for Lorem Ipsum. Lorem Ipsum may be used as a placeholder for any number of other characters",
       logo: "/images/client-logos/D.png",
     },
   ];
