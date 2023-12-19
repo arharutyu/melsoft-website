1c52b1aae8d9a3d50c6d3fc821a3bd8542682834
arharuty <13937@coderacademy.edu.au>
2023-12-19 14:05:29 +1100

    change p to div for correct DOM hierarchy

--- components/Process/Steps.js
+++ components/Process/Steps.js
@@ -7,40 +7,65 @@ const Steps = () => {
         <div className="col-lg-6 services-details">
           <div className="services-details-desc">
             <timeline>
-              <p>
+              <div>
                 <h3>Step 1: Discovery Chat</h3>
-                Let's talk about your vision! In a face-to-face or Zoom meeting,
-                we dive into your website goals and dreams.
-              </p>
-              <p>
+                <p>
+                  Let's talk about your vision! In a face-to-face or Zoom
+                  meeting, we dive into your website goals and dreams.
+                </p>
+              </div>
+              <div>
                 <h3>Step 2: Tailored Proposal</h3>
-                Within a week, we send you a proposal with theme options, color
-                palettes, and technical advice to shape your dream website.
-              </p>
-              <p>
+                <p>
+                  Within a week, we send you a proposal with theme options,
+                  color palettes, and technical advice to shape your dream
+                  website.
+                </p>
+              </div>
+              <div>
                 <h3>Step 3: Ready, Set, Build</h3>
-                With your green light, we start crafting the skeleton of your website, ensuring it matches your vision.
-              </p>
-              <p>
+                <p>
+                  With your green light, we start crafting the skeleton of your
+                  website, ensuring it matches your vision.
+                </p>
+              </div>
+              <div>
                 <h3>Step 4: Your Feedback Matters</h3>
-                You get at least 3 chances to share your thoughts as we refine the details, making sure it's precisely what you want.
-              </p>
-              <p>
+                <p>
+                  You get at least 3 chances to share your thoughts as we refine
+                  the details, making sure it's precisely what you want.
+                </p>
+              </div>
+              <div>
                 <h3>Step 5: Thorough Testing</h3>
-                We put your site through its paces, making sure it not only looks great but works flawlessly.
-              </p>
-              <p>
+                <p>
+                  We put your site through its paces, making sure it not only
+                  looks great but works flawlessly.
+                </p>
+              </div>
+              <div>
                 <h3>Step 6: Launch Readiness</h3>
-                When you're thrilled with the result, we schedule the big launch.
-              </p>
-              <p>
+                <p>
+                  When you're thrilled with the result, we schedule the big
+                  launch.
+                </p>
+              </div>
+              <div>
                 <h3>Step 7: Post-Launch Support</h3>
-                We provide easy-to-follow documentation for managing your content. Need extra help? One-on-one training is just a message away.
-              </p>
-              <p>
+                <p>
+                  We provide easy-to-follow documentation for managing your
+                  content. Need extra help? One-on-one training is just a
+                  message away.
+                </p>
+              </div>
+              <div>
                 <h3>Step 8: Ongoing Awesomeness</h3>
-                Choose our ongoing management for peace of mind, or take the reins yourself. We're here for free email support for the first 6 months.
-              </p>
+                <p>
+                  Choose our ongoing management for peace of mind, or take the
+                  reins yourself. We're here for free email support for the
+                  first 6 months.
+                </p>
+              </div>
             </timeline>
           </div>
         </div>
