ef34eec89ac85551469414bec18115c65632c8e2
arharuty <13937@coderacademy.edu.au>
2023-12-19 22:23:30 +1100

    add server components config

--- next.config.js
+++ next.config.js
@@ -6,6 +6,9 @@ module.exports = {
     },
     trailingSlash: true,
     optimizeFonts: false,
+    features: {
+        serverComponents: true,
+      },
     i18n: {
         locales: ['en', 'ar'],
         defaultLocale: 'en',
