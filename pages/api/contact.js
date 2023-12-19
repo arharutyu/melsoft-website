cf52d81951e17f5a20c8f947f641ab9d0abc7820
arharuty <13937@coderacademy.edu.au>
2023-12-19 14:07:37 +1100

    input API details and contact details

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,26 +3,24 @@ import sgTransport from "nodemailer-sendgrid-transport";
 
 const transporter = {
   auth: {
-    // Update your SendGrid API key here
-    api_key: "...",
+    api_key: process.env.SENDGRID_API_KEY,
   },
 };
 
 const mailer = nodemailer.createTransport(sgTransport(transporter));
 
 export default async (req, res) => {
-  // console.log(req.body)
   const { name, email, number, subject, text } = req.body;
 
   const data = {
-    // Update your email here
     to: "info@melsoft.com.au",
-    from: email,
+    from: "info@melsoft.com.au",
     subject: "Melsoft Equiry",
     text: text,
     html: `
         <b>From:</b> ${name} <br /> 
         <b>Number:</b> ${number} <br /> 
+        <b>Email:</b> ${email} <br /> 
         <b>Subject:</b> ${subject} <br /> 
         <b>Message:</b> ${text} 
     `,
